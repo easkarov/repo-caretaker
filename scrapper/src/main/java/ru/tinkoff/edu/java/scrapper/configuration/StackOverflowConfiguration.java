9311d828a563930a499ef32c3cc390a908459f34
EmiAsk <askarov.emii@yande.ru>
2023-03-25 18:09:30 +0300

    comment removed

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/configuration/StackOverflowConfiguration.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/configuration/StackOverflowConfiguration.java
@@ -5,8 +5,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 
 
-// Я хотел сделать конфигурацию таким образом, но в итоге решил не мудрить и бахнул @Value.
-// Хотелось бы узнать всё-таки каким-образом в этой ситуации сделать будет лучше.
 @ConfigurationProperties("stackoverflow-client")
 public record StackOverflowConfiguration(
         @DefaultValue("https://api.stackexchange.com/2.3") String baseUrl

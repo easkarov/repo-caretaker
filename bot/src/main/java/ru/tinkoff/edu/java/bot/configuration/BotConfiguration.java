0403a251a7e6c2e0c9788917be6638e81cf8dce7
EmiAsk <askarov.emii@yande.ru>
2023-04-01 14:33:38 +0300

    (feat) add new bean (Map<Command name, CommandHandler object>)

--- bot/src/main/java/ru/tinkoff/edu/java/bot/configuration/BotConfiguration.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/configuration/BotConfiguration.java
@@ -6,6 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.tinkoff.edu.java.bot.UpdatesProcessor;
+import ru.tinkoff.edu.java.bot.command.CommandHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
@@ -19,6 +24,17 @@ public class BotConfiguration {
         bot.setUpdatesListener(updatesProcessor);
         updatesProcessor.setBot(bot);
         return bot;
+    }
+
+    @Bean
+    // Вот этот бин потом можно будет заинжектить в CommandsHandler для более быстрого поиска команды что ли :/
+    //  Map command names to command objects for further injecting in CommandManager
+    public Map<String, CommandHandler<?, ?>> botCommands(List<CommandHandler<?, ?>> commandHandlers) {
+        var commandsMap = new HashMap<String, CommandHandler<?, ?>>();
+        for (var commandHandler : commandHandlers) {
+            commandsMap.put(commandHandler.command().name(), commandHandler);
+        }
+        return commandsMap;
     }
 
 }

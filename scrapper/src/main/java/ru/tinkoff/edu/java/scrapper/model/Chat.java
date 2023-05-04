fa6fbbfa02d552b6e7f2b03433ead0e3a9411b05
EmiAsk <askarov.emii@yande.ru>
2023-05-04 12:29:07 +0300

    (fix) fix tests

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/model/Chat.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/model/Chat.java
@@ -2,7 +2,7 @@ package ru.tinkoff.edu.java.scrapper.model;
 
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
@@ -15,6 +15,7 @@ import java.util.Set;
 @Getter
 @Setter
 @Accessors(chain = true)
+@EqualsAndHashCode(of = "id", callSuper = false)
 public class Chat {
 
     @Id

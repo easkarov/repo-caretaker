8c7df7cea0ab9c2e1b41934ede90818f5a4e71ce
EmiAsk <askarov.emii@yande.ru>
2023-05-04 11:58:29 +0300

    (fix) little fixes

--- bot/src/main/java/ru/tinkoff/edu/java/bot/configuration/ApplicationConfig.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/configuration/ApplicationConfig.java
@@ -5,16 +5,14 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
-@ConfigurationProperties(prefix = "app")
+@ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 public record ApplicationConfig(
         @NotNull String test,
         @NotNull Bot bot,
         @NotNull ScrapperClient client
 ) {
 
-    public record ScrapperClient(@NotNull String url) {
-    }
+    public record ScrapperClient(String url) {}
+    public record Bot(String token) {}
 
-    public record Bot(@NotNull String token) {
-    }
 }
